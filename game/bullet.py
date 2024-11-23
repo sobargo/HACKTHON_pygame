123195a43b471be8e7ca3e9db71ee2625beee762
sobargo <3501096663@qq.com>
2024-11-23 20:26:58 +0800

    Update bullet.py

--- game/bullet.py
+++ game/bullet.py
@@ -1,4 +1,8 @@
 import pygame
-class Bullet:
-    def __init__(self):
-        pass
+from pygame.sprite import  Sprite
+class Bullet(Sprite):
+    def __init__(self,tw_game):
+        super().__init__()
+        self.screen = tw_game.screen
+        self.settings = tw_game.settings
+        
