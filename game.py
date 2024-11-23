22fb5fda61e93cd5db0172538f29b7125fcae259
sobargo <3501096663@qq.com>
2024-11-23 11:22:35 +0800

    Update game.py

--- game.py
+++ game.py
@@ -1,1 +1,15 @@
-print("hello,world")
+import sys
+import pygame
+class tank_war:
+    def __init__(self):
+        self.screen = pygame.display.set_mode((1200,800))
+        pygame.display.set_caption("Tank War")
+    def run_game(self):
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    sys.exit()
+            pygame.display.flip()
+if __name__ == '__main__':
+    tw = tank_war()
+    tw.run_game()            
