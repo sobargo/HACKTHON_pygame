e74c6f2e284bde3c7da78d6c7cf536d6dbbc0851
Lazist-09 <15284162817@163.com>
2024-11-24 22:40:25 +0800

    车身
    更新车身旋转
    更改子弹大小

--- game/player_tank.py
+++ game/player_tank.py
@@ -1,4 +1,5 @@
 import pygame
+import math
 from settings import Settings
 class Player_tank:
     #=====车身四种变换=====
@@ -13,7 +14,7 @@ class Player_tank:
         self.screen_rect = tw_game.screen.get_rect()
 
 
-        self.image = pygame.image.load(r"asset\image\map_Obstacles_png\tank1_body_u.png")
+        self.image = pygame.image.load(r"asset\image\map_Obstacles_png\tank1_body_r.png")
         #默认车身向上
         self.rect = self.image.get_rect()
 
@@ -24,25 +25,27 @@ class Player_tank:
         self.moving_left = False
         self.moving_up = False
         self.moving_down = False
-
+        self.angle = 0  # 初始旋转角度为0
 
 
 
     def update(self):
         if self.moving_right and self.rect.right<self.screen_rect.right:
-            self.image = pygame.image.load(r"asset\image\map_Obstacles_png\tank1_body_r.png")
-            self.x+=self.settings.player_tank_speed
+            self.angle += 0.1
+            self.image = pygame.transform.rotate(self.image, self.angle)
+            self.rect = self.image.get_rect(center=self.rect.center)
         if self.moving_left and self.rect.left>0:
-            self.image =   pygame.image.load(r"asset\image\map_Obstacles_png\tank1_body_l.png")
-            self.x-=self.settings.player_tank_speed
+            self.angle -= 0.1
+            self.image = pygame.transform.rotate(self.image, self.angle)
+            self.rect = self.image.get_rect(center=self.rect.center)
         if self.moving_down and self.rect.bottom<self.screen_rect.bottom:
-            self.image =  pygame.image.load(r"asset\image\map_Obstacles_png\tank1_body_d.png")
-            self.y+=self.settings.player_tank_speed
+            self.rect.x -= 1.5 * math.cos(math.radians(self.angle))
+            self.rect.y += 1.5 * math.sin(math.radians(self.angle))
         if self.moving_up and self.rect.top>0:
-            self.image =pygame.image.load(r"asset\image\map_Obstacles_png\tank1_body_u.png")
-            self.y-=self.settings.player_tank_speed
-        self.rect.x = self.x
-        self.rect.y = self.y
+            self.rect.x += 1.5 * math.cos(math.radians(self.angle))
+            self.rect.y -= 1.5 * math.sin(math.radians(self.angle))
+        # self.rect.x = self.x
+        # self.rect.y = self.y
         
     def blitme(self):
         self.screen.blit(self.image,self.rect)
