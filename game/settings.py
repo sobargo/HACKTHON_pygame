9d4379ed5b0541d629d8f30e7d3315feb0d5c36e
sobargo <3501096663@qq.com>
2024-11-26 20:13:10 +0800

    1

--- game/settings.py
+++ game/settings.py
@@ -1,7 +1,7 @@
 import pygame
 class Settings:
     def __init__(self):
-        self.player_tank_speed = 1.5
+        self.player_tank_speed = 5
         self.screen_width = 1920
         self.screen_height = 1080
         self.bullet_speed = 2.0
